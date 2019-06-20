5d0f7bfbee9fdad0aa95b2242a4e62a01549106a
Giancarlo <giancarloct90@hotmail.com>
2019-06-20 14:25:30 -0600

    por si falla

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -13,7 +13,32 @@ const Usuario = require('../models/usuario');
 // estos son los midleware que estamos usando para que maneje nuestra apliacion
 // las peticiones que manjean nuestra app
 app.get('/usuario', function (req, res) {
-    res.json('get usuario')
+    // en el siguiente codigo estamos obteniendo todos los datos del modelo usuario
+    // aqui estamos obteniendo lo valores que el usaurio manda a travez de link para poder establecer un limite y un desde, ejemplo localhos:3000/usuario/?nombre=jack&&limite=44
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    // con el comando find obtenemos los datos de toda los usuarios, find recibe un condicion {} que significa todos los campos
+    // la funcion skip recibe un paramtro entero y quiero decir desde el registro x trae informacion
+    // la funcion limit tambien recibe un entero como parametro, y es el limite hasta donde deberia de llegar
+    // exec es para que se ejecute y recibe un callback, con un erro o un objeto lleno de objetos que ese encuantran en lad db
+    Usuario.find({}, 'nombre email role estado google img').skip(desde).limit(limite).exec((err, usuarios)=>{
+        if(err){
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        // ejecutando la funcion count recibimos cuantas registros hay en nuestra coleccion
+        Usuario.count({}, (err, conteo)=>{
+            res.json({
+                ok:true,
+                usuarios,
+                cuantos: conteo
+            });
+        });
+    });
 })
 
 app.post('/usuario', function (req, res) {
@@ -72,11 +97,14 @@ app.put('/usuario/:id', function (req, res) {
     // de esta manera obtenemos el id que viene en el link 
     let id = req.params.id;
     // con este comando obtenemos todo el form que nos envian
+
+    // underscore es un alibreria que expande las funciones de js, con esta funcion devuelve el objeto solo con los objeto que queremos ver, solo con los objetos que necesitamos
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
     // usamos un metodo de nuestro modelo previamnete creado, que se llama findOneAndUpdate, que recibe 4 parametros en este caso
     // el primer parametro es el id, que lo va a buscar en la base de datos y si existe vaa seguir con la modificacion,
     // el segundo parametro es el body es los nuevos datos que recibimos del frontend, el tercer parametro son unas opciones de la funcion findOneAndUpdate, 
     // en el tercer parametro nosotros les estamos enviando un objeto con la propiedad new igual a true, esto nos sirve para que nos regrese el nuesvo objeto que modifico
+    // en el tercer parametro donde recibimos opciones para ejecurtar la funcion, podemos poner la opcion runValidator y eso va hacer que las validaciones se corran en este momento las validaciones que pusimo en nuestro modelo
     //el 4to parametro es un callback que recibe dos parametros, el error en caso de ue suceda un error y el objeto modificado si lo modifico.
     Usuario.findOneAndUpdate(id, body, {
         new: true,
@@ -95,8 +123,27 @@ app.put('/usuario/:id', function (req, res) {
     })
 })
 
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
+app.delete('/usuario/:id', function (req, res) {
+    //res.json('delete usuario')
+    let id = req.params.id;
+    Usuario.findByIdAndRemove(id,(err, usuarioBorrado)=>{
+        if(err){
+            return res.status(400).json({
+               ok: false,
+               err 
+            });
+        }
+        if(usuarioBorrado === null){
+            return res.status(400).json({
+                ok: true,
+                err
+            });
+        }
+        res.json({
+            ok:true,
+            usuarioBorrado
+        });
+    });
 })
 
 module.exports = app;
