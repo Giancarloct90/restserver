34ebd9c4b7624e59e8fc48d53efa33544a12a05a
Giancarloct90 <giancarloct90@hotmail.com>
2019-06-24 21:39:07 -0600

    Token casa 2

--- server/middlewares/autentificacion.js
+++ server/middlewares/autentificacion.js
@@ -7,17 +7,17 @@ const jwt = require('jsonwebtoken');
 // este es un middleware, que basicamente lo que hace es verificar la firma de nuestro token
 // verificarToken es una funcion que recibe 3 parametros req y res, son parametros que vienen de la funccion get
 
-let verificarToken = (req, res, next)=>{
+let verificarToken = (req, res, next) => {
     // con este codigo estamos obteniendo el token que viene en los headers
     let token = req.get('token');
     // la funcion verify, recibe el parametros
     // 1 parametro: es el token que obtenemos que nos envia la peticion
     // 2 parametro: es la firma que nosotros creamos o usamos,
     // 3 parametro: es la funcion anonima con la que con la que manejamos el error y el resultado de la veriifiacion de firma
-    jwt.verify(token,process.env.SEED ,(err, decoded)=>{
-        if(err){
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
             return res.status(401).json({
-                ok:false,
+                ok: false,
                 err
             });
         }
@@ -26,9 +26,9 @@ let verificarToken = (req, res, next)=>{
         req.usuario = decoded.usuario
         next();
     });
-   // res.json({
-   //     token
-   // });
+    // res.json({
+    //     token
+    // });
 };
 
 module.exports = {
