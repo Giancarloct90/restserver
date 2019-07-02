89c818e5e165a146312f851efd8659a601defd81
Giancarloct90 <giancarloct90@hotmail.com>
2019-07-01 21:41:02 -0600

    Producto

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -13,7 +13,13 @@ const {
 // Obtener todos las Categorias
 //==========================
 app.get('/categoria', [verificarToken], (req, res) => {
-    Categoria.find({}).exec((err, categorias) => {
+    // la coleccion categoria tiene el objectid de con el id de la coleccion usuario, es una especie de relacion uno a muchos,
+    // en los datos de la coleccion categoria tenemos un is de usuario que hace una especie de referencia a un registro de la coleccion usuario
+    // con la funcion populate obtenemos todos los datos de la coleccion de usuario, la funcion populate recibe varios parametros pero en este caso nosotros le estamos mandado dos parametros 
+    // 1 parametro: es el nombre de la coleecion de la cual necesitamos inportar los datos
+    // 2 parametro: son las propiedades, que queremos que se muestren
+    // con la funcion sort, ordenamos la coleccion con la descripcion
+    Categoria.find({}).sort('descripcion').populate('usuario', 'nombre email').exec((err, categorias) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
@@ -34,24 +40,26 @@ app.get('/categoria', [verificarToken], (req, res) => {
 //==========================
 // Mostrar una categoria por id
 //==========================
-// app.get('/categoria:id', {
-//     verificarToken
-// }, (req, res) => {
-//     let id = req.body.id
-//     Categoria.findById(id, ).exec((err, categoria) => {
-//         if (err) {
-//             return res.status(400).json({
-//                 ok: false,
-//                 err
-//             });
-//         }
-//         res.json({
-//             ok: true,
-//             categoria
-//         });
-//     });
+app.get('/categoria/:id', verificarToken,
+    (req, res) => {
+        let id = req.params.id
+        Categoria.findById(id, (err, categoria) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            if (!categoria) {
+                return re
+            }
+            res.json({
+                ok: true,
+                categoria
+            });
+        });
 
-// });
+    });
 
 //==========================
 // Insertar una categoria nueva
@@ -67,7 +75,14 @@ app.post('/categoria',
 
         categoria.save((err, categoriaDB) => {
             if (err) {
-                res.status(400).json({
+                res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            // si no se creo la categoria, responda con este msj
+            if (!categoriaDB) {
+                return res.status(400).json({
                     ok: false,
                     err
                 });
@@ -85,34 +100,39 @@ app.post('/categoria',
 //==========================
 // Actualizar uan categoria
 //==========================
-app.put('/categoria/:id', [verificarToken,
-    verificaAdmin_Role
-], (req, res) => {
-    let id = req.params.id;
+app.put('/categoria/:id', verificarToken,
+    (req, res) => {
+        let id = req.params.id;
 
-    // con la funcione pick lo que estamos haciendo es que regresa, un objeto pero solo con la propiedades que nosotros le estamos espicificando
-    // el primer parametro es el objeto que queremos que nos devuelva
-    // el segundo parametro son las propiedades del objeto que queremos que nos devuelva
-    //let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
-    let body = req.body;
+        // con la funcione pick lo que estamos haciendo es que regresa, un objeto pero solo con la propiedades que nosotros le estamos espicificando
+        // el primer parametro es el objeto que queremos que nos devuelva
+        // el segundo parametro son las propiedades del objeto que queremos que nos devuelva
+        //let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+        let body = req.body;
 
-    Categoria.findOneAndUpdate(id, body, {
-        new: true,
-        runValidators: true
-    }, (err, categoriaDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err
+        Categoria.findOneAndUpdate(id, body, {
+            new: true,
+            runValidators: true
+        }, (err, categoriaDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            if (!categoriaDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                categoriaDB
             });
-        }
-        res.json({
-            ok: true,
-            categoriaDB
         });
-    });
 
-});
+    });
 
 
 //==========================
